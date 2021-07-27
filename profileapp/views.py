c515e2d287a1456ee350b1977453f4733260cb0b
leeseungjin <pelor7317@likelion.org>
2021-07-27 10:50:37 +0900

    7/27 class3 commit 3

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import render
 
 # Create your views here.
@@ -13,3 +14,7 @@ class ProfileCreateView(CreateView):
     form_class = ProfileCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
