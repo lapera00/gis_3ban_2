2dc83c87aa4ba015ec13dffa6addda9b55b9f840
leeseungjin <pelor7317@likelion.org>
2021-07-27 10:04:23 +0900

    7/27 class3 commit 2

--- profileapp/models.py
+++ profileapp/models.py
@@ -8,4 +8,4 @@ class Profile(models.Model):
 
     image = models.ImageField(upload_to='profile/', null=True)
     nickname = models.CharField(max_length=30, unique=True, null=True)
-    massage = models.CharField(max_length=200, null=True)
+    message = models.CharField(max_length=200, null=True)
